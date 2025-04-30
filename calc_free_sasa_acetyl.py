29251658632332eb70135a5f69e21de612bf3a74
Jack Shaw <112831590+JackShaw1@users.noreply.github.com>
2025-04-30 12:33:32 -0400

    Update calc_free_sasa_acetyl.py

--- calc_free_sasa_acetyl.py
+++ calc_free_sasa_acetyl.py
@@ -14,18 +14,19 @@ with open('holder_csv.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         if row[0] != 'GENE':
+            if f'{key}.pdb' in os.listdir('pdb_files'):
             structure = parser.get_structure('struct', f'pdb_files/{row[1]}.pdb') 
-            # ns = NeighborSearch(list(structure.get_atoms()))
-            sr = ShrakeRupley(probe_radius=1.4, n_points=100)
-            sr.compute(structure, level='A')
-            for model in structure:
-                for chain in model:
-                    for residue in chain:
-                        if residue.get_id()[1] == int(row[2]):
-                            print(residue.get_resname())
-                            for atom in residue:
-                                if atom.get_name() == 'NZ':
-                                    with open('out_with_sasa.csv', 'a', newline='') as outfile:
-                                        writer = csv.writer(outfile)
-                                        writer.writerow(row + [atom.sasa])
+                # ns = NeighborSearch(list(structure.get_atoms()))
+                sr = ShrakeRupley(probe_radius=1.4, n_points=100)
+                sr.compute(structure, level='A')
+                for model in structure:
+                    for chain in model:
+                        for residue in chain:
+                            if residue.get_id()[1] == int(row[2]):
+                                print(residue.get_resname())
+                                for atom in residue:
+                                    if atom.get_name() == 'NZ':
+                                        with open('out_with_sasa.csv', 'a', newline='') as outfile:
+                                            writer = csv.writer(outfile)
+                                            writer.writerow(row + [atom.sasa])
 
